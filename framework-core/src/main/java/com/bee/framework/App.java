fd8a497e3ef01ca1aff6d51df49507505349565d
jaco <huangzhixion51@126.com>
2018-09-20 22:54:59 +0800

    Initial commit

---
+++ framework-core/src/main/java/com/bee/framework/App.java
@@ -0,0 +1,13 @@
+package com.bee.framework;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
